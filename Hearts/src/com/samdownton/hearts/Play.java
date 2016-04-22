17dc925996c50b0d2436422fa38f43e42e270cb4
samuel.downton <samuel.downton@metoffice.gov.uk>
2016-04-22 12:54:22 +0100

    Refactor code so player who looses last round goes first in next round.

--- Hearts/src/com/samdownton/hearts/Play.java
+++ Hearts/src/com/samdownton/hearts/Play.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 public class Play {
 	private static final Scanner in = new Scanner(System.in);
-	private int lastWinner;
+	private int lastLoser;
 
 	public void playGame() {
 
-		System.out.print("Please enter player one name: ");
-		Player player1 = new Player(in.next());
-		System.out.print("Please enter player two name: ");
-		Player player2 = new Player(in.next());
-		System.out.print("Please enter player three name: ");
-		Player player3 = new Player(in.next());
-		System.out.print("Please enter player four name: ");
-		Player player4 = new Player(in.next());
-		System.out.println("Players are: " + player1.getName() + ", " + player2.getName() + ", " + player3.getName()
+		System.out.print("Please enter your name: ");
+		
+		Player human = new Player(in.next());
+		Player player2 = new Player("Scarlet");
+		Player player3 = new Player("Jane");
+		Player player4 = new Player("Paul");
+		System.out.println("Players are: " + human.getName() + ", " + player2.getName() + ", " + player3.getName()
 				+ ", " + player4.getName() + ".");
+		
 		ArrayList<Player> players = new ArrayList<>();
-		players.add(player1);
+		players.add(human);
+		lastLoser = 0;
 		players.add(player2);
 		players.add(player3);
 		players.add(player4);
@@ -34,13 +34,15 @@ public class Play {
 		}
 
 		while (players.get(0).getHand().size() > 0 && players.get(1).getHand().size() > 0 && players.get(2).getHand().size() > 0 && players.get(3).getHand().size() > 0){
-			showPlayerCards(player1);
+			showPlayerCards(human);
 			playRound(players);
 		}
+		
 		for (Player player : players){
 			int score = player.calculateScore();
 			System.out.println(player.getName() + "'s score is: " + score);
 		}
+		
 		findWinner(players);
 		
 	}
@@ -57,21 +59,47 @@ public class Play {
 	}
 
 	private void playRound(ArrayList<Player> players) {
-		System.out.println("Select card to play: ");
-		int cardToPlay = in.nextInt();
+		
 		ArrayList<Card> onTable = new ArrayList<>();
-		onTable.add(players.get(0).getHand().get(cardToPlay));
-		players.get(0).getHand().remove(cardToPlay);
-		for (int i = 1; i < players.size(); i++) {
-			onTable.add(players.get(i).getHand().get((players.get(i).getHandSize()) - 1));
-			players.get(i).getHand().remove(players.get(i).getHandSize() - 1);
+		if (lastLoser == 0){
+			System.out.println();
+			System.out.print("Select card to play: ");
+			int cardToPlay = in.nextInt();
+			onTable.add(players.get(0).getHand().get(cardToPlay));
+			players.get(0).getHand().remove(cardToPlay);
+			for (int i = 1; i < players.size(); i++) {
+				onTable.add(players.get(i).getHand().get((players.get(i).getHandSize()) - 1));
+				players.get(i).getHand().remove(players.get(i).getHandSize() - 1);
+			}
+			System.out.println(Card.printCards(onTable));
 		}
-		System.out.println(Card.printCards(onTable));
-
-		String suit = onTable.get(0).getSuit();
-		int max = onTable.get(0).getFaceValue();
+		else {
+			for (int i = lastLoser; i < 4; i++){
+				onTable.add(players.get(i).getHand().get((players.get(i).getHandSize())-1));
+				players.get(i).getHand().remove(players.get(i).getHandSize() - 1);
+			}
+			
+			System.out.println();
+			System.out.println("Cards played so far" + Card.printCards(onTable));
+			System.out.println();
+			System.out.println("Select card to play: ");
+			int cardToPlay = in.nextInt();
+			onTable.add(0, players.get(0).getHand().get(cardToPlay));
+			players.get(0).getHand().remove(cardToPlay);
+			
+			for (int i = 1; i < lastLoser; i++){
+				onTable.add(i, players.get(i).getHand().get((players.get(i).getHandSize())-1));
+				players.get(i).getHand().remove(players.get(i).getHandSize() - 1);
+			}
+			
+			System.out.println();
+			System.out.println( Card.printCards(onTable));
+		}
+		
+		String suit = onTable.get(lastLoser).getSuit();
+		int max = 0;
 		int maxCard = 0;
-		for (int i = 1; i < onTable.size(); i++) {
+		for (int i = 0; i < onTable.size(); i++) {
 			if (onTable.get(i).getSuit().equals(suit)) {
 				if (max < onTable.get(i).getFaceValue()) {
 					max = onTable.get(i).getFaceValue();
@@ -79,10 +107,13 @@ public class Play {
 				}
 			}
 		}
+		
+		lastLoser = maxCard;
 		System.out.println(players.get(maxCard).getName() + " looses this round!");
 		for (Card card : onTable) {
 			players.get(maxCard).addToReceived(card);
 		}
+		
 
 	}
 	private void findWinner(ArrayList<Player> players){
